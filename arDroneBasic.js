fd8c5ad66024e4510ef02b532af5a12471caf256
albertJ32 <00b0d14c@opayq.com>
2015-10-22 22:46:22 +0100

    slightly more update code

--- arDroneBasic.js
+++ arDroneBasic.js
@@ -1,22 +1,21 @@
-// Parent Program
-var iteration = 0;
+    // Parent Program
+    var iteration = 0;
 
-function startProgram(){
-  var exec  = require('child_process').exec;
-    exec('sh myscript.sh', function(error, stdout, stderr){
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
-      if(error += null){
-        console.log('exec ERROR: ' + error);
-      }
+    function startProgram(){
+      var exec  = require('child_process').exec;
+        exec('sh myscript.sh', function(error, stdout, stderr){
+          console.log('stdout: ' + stdout);
+          console.log('stderr: ' + stderr);
+          if(error += null){
+            console.log('exec ERROR: ' + error);
+          }
 
-      console.log('Getting Image');
-      exec('node savePNG.js',function(error, stdout, stderr){
-        console.log('IMage collected..');
-      });
-    });
-}
-
-setInterval(function(){
-  startProgram();
-}, 5000);
+          console.log('Getting Image');
+          exec('node savePNG.js',function(error, stdout, stderr){
+            console.log('IMage collected..');
+          });
+        });
+    }
+    setInterval(function(){
+      startProgram();
+    }, 5000);
--- savePNG.js
+++ savePNG.js
@@ -1,28 +1,28 @@
 
-  var arDrone = require('ar-drone');
-  var client = arDrone.createClient();
-  var fs = require('fs');
+    var arDrone = require('ar-drone');
+    var client = arDrone.createClient();
+    var fs = require('fs');
 
-  var pngStream = client.getPngStream();
-  var frameCounter = 0;
-  var saveDir = './Images/'; // Image Dir
+    var pngStream = client.getPngStream();
+    var frameCounter = 0;
+    var saveDir = './Images/'; // Image Dir
 
-  pngStream
-    .on('error', console.log)
-    .on('data', function(pngBuffer) {
-      // Exit after first image saved
-      if(frameCounter!=0){
-        process.exit();
-      }
-
-      // Create ImageName and save to dir
-      var imageName = saveDir + 'frame' + '.png';
-      fs.writeFile(imageName, pngBuffer, function(err) {
-        if (err) {
-          console.log('Error saving PNG: ' + err);
+    pngStream
+      .on('error', console.log)
+      .on('data', function(pngBuffer) {
+        // Exit after first image saved
+        if(frameCounter!=0){
+          process.exit();
         }
-      });
-      console.log(imageName); // Output imagePath for use in parent
 
-      frameCounter++;
-    });
+        // Create ImageName and save to dir
+        var imageName = saveDir + 'frame' + '.png';
+        fs.writeFile(imageName, pngBuffer, function(err) {
+          if (err) {
+            console.log('Error saving PNG: ' + err);
+          }
+        });
+        console.log(imageName); // Output imagePath for use in parent
+
+        frameCounter++;
+      });
